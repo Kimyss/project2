0406798df9585cf5f0cb54e7d11a7235908abf9e
'kimyss' <'kimyslive7@naver.com'>
2024-03-26 11:37:06 +0900

    간단수정
    특이사항없음
    (참고)최초 푸쉬는 commit먼저

--- src/quiz/Quiz7.java
+++ src/quiz/Quiz7.java
@@ -7,10 +7,10 @@ public class Quiz7 {
 		double d = 1.2;
 		float f = 0.9f;
 		
-		int i = (int)d;  //1
-		int i2 = (int)f;  //1
-		int i3 = (int)d + (int)f; //2
-		int i4 = (int)(d+f);  // 1.2+ 0.9 = 2.1  -> 2
+		int i = (int)d;  //
+		int i2 = (int)f;  // 반올림이 안되네?
+		int i3 = (int)d + (int)f; // 형변환을 각각 한 후에 합
+		int i4 = (int)(d+f);  // 더한 후에 형변환 1.2+ 0.9 = 2.1  -> 2
 		
 		System.out.println(i);
 		System.out.println(i2);
